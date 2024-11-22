3499b2a03b8c2eb801e279b60bde0d247faf9a50
shredder1970 <nelson.delpozo.dev@gmail.com>
2024-11-22 02:57:18 -0500

    fixed titles

--- app/routes/policies.privacy.tsx
+++ app/routes/policies.privacy.tsx
@@ -1,17 +1,11 @@
-import type { MetaFunction } from "@remix-run/node";
-
-export const meta: MetaFunction = () => [{ title: "Help" }];
-
 export default function Privacy() {
   return (
     <main className="flex flex-grow flex-col items-center justify-center bg-white">
       <div className="mx-auto w-full max-w-2xl px-8 pb-16">
-        <h1 className="text-gray font-pressStart text-center text-2xl font-bold">
+        <h1 className="text-gray text-center font-pressStart text-2xl font-bold">
           Privacy Policy
         </h1>
-        <h2 className="mt-5 text-lg font-bold">
-          Scope of this Policy
-        </h2>
+        <h2 className="mt-5 text-lg font-bold">Scope of this Policy</h2>
         <p className="text-md mt-4 text-gray-800">
           This Privacy Policy applies to your use of our website and our mobile
           applications operated by RS-PRO LLC . This Privacy Policy also applies
@@ -25,12 +19,12 @@ export default function Privacy() {
         <h2 className="mt-5 text-lg font-bold">
           Personal Information We Collect
         </h2>
-        <p className="mt-4 text-md text-gray-700">
+        <p className="text-md mt-4 text-gray-700">
           Your contact details and account data if you establish an online
           account with us (such as your name, email address, mailing address,
           phone number, and password).
         </p>
-        <p className="mt-4 text-md text-gray-700">
+        <p className="text-md mt-4 text-gray-700">
           Your payment information if you make a purchase via the site. We use
           third-party payment processors, such as Stripe, to process your
           payments. These third parties will collect the information necessary
@@ -41,19 +35,29 @@ export default function Privacy() {
           information.
         </p>
         <h2 className="mt-5 text-lg font-bold">
-        How We Use Personal Information
+          How We Use Personal Information
         </h2>
-        <p className="mt-4 text-md text-gray-700">
-        Providing and supporting our services: We use personal information to operate, maintain, and provide you with our services. In particular, we will use personal information to perform our contractual obligation under our terms of use, such as to allow you to create an account and make purchases via the site, and for us to take payment and deliver your purchases.
+        <p className="text-md mt-4 text-gray-700">
+          Providing and supporting our services: We use personal information to
+          operate, maintain, and provide you with our services. In particular,
+          we will use personal information to perform our contractual obligation
+          under our terms of use, such as to allow you to create an account and
+          make purchases via the site, and for us to take payment and deliver
+          your purchases.
         </p>
-        <p className="mt-4 text-md text-gray-700">
-        Communicate with you about our services: It is in our legitimate business interests to use personal information to respond to your requests, provide customer support, and communicate with you about our Services, including by sending announcements, updates, security alerts, and support and administrative messages, and to administer surveys, sweepstakes, and contests.
+        <p className="text-md mt-4 text-gray-700">
+          Communicate with you about our services: It is in our legitimate
+          business interests to use personal information to respond to your
+          requests, provide customer support, and communicate with you about our
+          Services, including by sending announcements, updates, security
+          alerts, and support and administrative messages, and to administer
+          surveys, sweepstakes, and contests.
         </p>
         <h2 className="mt-5 text-center text-lg font-bold">
-        RS-PRO does not share your personal information with anyone for any reason.
+          RS-PRO does not share your personal information with anyone for any
+          reason.
         </h2>
       </div>
     </main>
   );
 }
-
